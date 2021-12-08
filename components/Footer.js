bae710cd90f96c013068c6b5b2eff73a349eb6c9
Helen Mathew <helenn.mathew@gmail.com>
2021-12-08 04:12:56 +0400

    footer github goes to repo

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,11 @@ import tools from "../data/tools/icons.json";
 const Footer = () => {
     return (
         <footer className="flex justify-center items-center font-normal font-inter text-gray-200">
-            <a href="https://github.com/helen-mathew" className="mt-1">
+            <a
+                href="https://github.com/helen-mathew/portfolio"
+                className="mt-1"
+                target="_blank"
+            >
                 <ion-icon
                     name="logo-github"
                     style={{fontSize: "40px"}}
